80c425d4f195f8e514feafa43c7ef0f4f3d2d20a
Victor Ryan <vryan9615@gmail.com>
2022-04-07 17:09:12 -0400

    Updated Data and Models project

---
+++ SP.Data/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Data
+{
+    public class Ship
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTimeOffset Created_At { get; set; }
+        [ForeignKey(nameof(Origin))]
+        [Required]
+        public string OriginName { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+        [Required]
+        public string ShipSize { get; set; } //Maybe an int?
+        [Required]
+        public string ShiPurpose { get; set; }
+        [Required]
+        public string CaptainName { get; set; }
+        [Required]
+        public int CrewSize { get; set; }
+        [Required]
+        public string Capacity { get; set; }
+        [Required]
+        public string TopSpeed { get; set; } //Also maybe an int?
+    }
+}
---
+++ SP.Models/ShipListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Models
+{
+    public class ShipListItem
+    {
+        //Hello! To be filled out at a later date.
+    }
+}
