adac2f321e7a7c7673d7fa71ec0b41caee8e9b8a
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-08 10:15:22 -0400

    first commit

---
+++ SP.Data/Origin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Data
+{
+    public class Origin
+    {
+        [Key]
+        public int OriginId { get; set; }
+        [Required]
+        public string OriginName { get; set; }
+        [Required]
+        public DateTimeOffset CreationDate { get; set; }
+        [Required]
+        public int RegisteredShips { get; set; }
+    }
+}
