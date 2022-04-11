a58ca890ee76d31456d10a6b96c38c88c7d10016
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-11 14:27:36 -0400

    origin controller made

---
+++ SP.WebApi/Controllers/OriginController.cs
@@ -0,0 +1,59 @@
+﻿using SP.Models;
+using SP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SP.WebApi.Controllers
+{
+    public class OriginController : ApiController
+    {
+        private OriginService CreateOriginService()
+        {
+            
+            var originService = new OriginService();
+            return originService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            OriginService originService = CreateOriginService();
+            var origin = originService.GetOrigin();
+            return Ok(origin);
+        }
+
+        public IHttpActionResult Post(OriginCreate origin)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOriginService();
+
+            if (!service.CreateOrigin(origin))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            OriginService originService = CreateOriginService();
+            var origin = originService.GetOriginById(id);
+            return Ok();
+        }
+        public IHttpActionResult Put(OriginEdit origin)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOriginService();
+
+            if (!service.UpdateOrigin(origin))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
