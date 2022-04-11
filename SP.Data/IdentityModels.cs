668bffa1e71ad46a8cb668c81f117c0ef6254edb
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-11 15:25:50 -0400

    comment out other dbsets

--- SP.Data/IdentityModels.cs
+++ SP.Data/IdentityModels.cs
@@ -33,7 +33,8 @@ namespace SP.Data
             return new ApplicationDbContext();
         }
         public DbSet<Origin> Origin { get; set; }
-        
+        //public DbSet<Ship> Ships { get; set; }
+        //public DbSet<Crew> Crew { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
