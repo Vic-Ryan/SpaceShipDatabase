d728d9d9e051f5510cb3b46859f242a592032702
avery finchum <finchum.avery@gmail.com>
2022-04-12 15:54:56 -0400

    added edit crew

---
+++ SP.Models/CrewEdit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Models
+{
+    public class CrewEdit
+    {
+        public int CrewId { get; set; }
+
+        public DateTimeOffset Created_At { get; set; }
+
+        public Guid OwnerId { get; set; }
+        public string Ship { get; set; }
+        //[ForeignKey(nameof(Origin))]
+        public virtual string OriginName { get; set; }
+        public string CrewName { get; set; }
+        public string CrewRole { get; set; }
+        public string CrewDescription { get; set; }
+        public string CrewMembers { get; set; }
+    }
+}
