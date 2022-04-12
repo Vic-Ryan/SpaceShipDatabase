8e089f581e4722480347d76946fc01d0efd547d2
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-12 10:21:56 -0400

    testing ships

--- SP.Models/ShipCreate.cs
+++ SP.Models/ShipCreate.cs
@@ -1,24 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SP.Data
+namespace SP.Models
 {
-    public class Ship
+    public class ShipCreate
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public Guid OwnerId { get; set; }
         [Required]
         public string ShipName { get; set; }
-        public DateTimeOffset Created_At { get; set; }
-        //[ForeignKey(nameof(Origin))]
-        public int? OriginId { get; set; }
-        //public virtual Origin Origin { get; set; } 
         [Required]
         public string Manufacturer { get; set; }
         [Required]
-        public string ShipSize { get; set; } //Maybe an int?
+        public string ShipSize { get; set; }
         [Required]
         public string ShipPurpose { get; set; }
         [Required]
@@ -28,6 +24,6 @@ namespace SP.Data
         [Required]
         public string Capacity { get; set; }
         [Required]
-        public string TopSpeed { get; set; } //Also maybe an int?
+        public string TopSpeed { get; set; }
     }
 }
--- SP.Models/ShipDetail.cs
+++ SP.Models/ShipDetail.cs
@@ -1,33 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SP.Data
+namespace SP.Models
 {
-    public class Ship
+    public class ShipDetail
     {
-        [Key]
+
         public int Id { get; set; }
-        [Required]
         public Guid OwnerId { get; set; }
-        [Required]
         public string ShipName { get; set; }
+        [Display(Name = "Created")]
         public DateTimeOffset Created_At { get; set; }
-        //[ForeignKey(nameof(Origin))]
         public int? OriginId { get; set; }
-        //public virtual Origin Origin { get; set; } 
-        [Required]
         public string Manufacturer { get; set; }
-        [Required]
-        public string ShipSize { get; set; } //Maybe an int?
-        [Required]
+        public string ShipSize { get; set; }
         public string ShipPurpose { get; set; }
-        [Required]
         public string CaptainName { get; set; }
-        [Required]
         public int CrewSize { get; set; }
-        [Required]
         public string Capacity { get; set; }
-        [Required]
-        public string TopSpeed { get; set; } //Also maybe an int?
+        public string TopSpeed { get; set; }
     }
 }
