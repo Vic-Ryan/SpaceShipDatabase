30683da678afdeb02b16d5aa66db931cfccbe167
avery finchum <finchum.avery@gmail.com>
2022-04-12 14:29:21 -0400

    Updated Crew Props

--- SP.Data/Crew.cs
+++ SP.Data/Crew.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,21 @@ namespace SP.Data
 {
     public class Crew
     {
+        [Key]
+        public int CrewId { get; set; }
+        [Required]
+        public DateTimeOffset Created_At { get; set; }
+        [Required]
+        public string Ship { get; set; }
 
+        [ForeignKey(nameof(Origin))]
+        [Required]
+        public string OriginName { get; set; }
+        [Required]
+        public string CrewName { get; set; }
+        public string CrewRole { get; set; }
+        public string CrewDescription { get; set; }
+        public string CrewMembers { get; set; }
+       
     }
 }
