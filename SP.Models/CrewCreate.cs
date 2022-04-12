44554868b794985a40810b541c15cb4518ca30e4
avery finchum <finchum.avery@gmail.com>
2022-04-12 15:20:46 -0400

    create crew create

---
+++ SP.Models/CrewCreate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Models
+{
+    public class CrewCreate
+    {
+        [Key]
+        public int CrewId { get; set; }
+        [Required]
+        public DateTimeOffset Created_At { get; set; }
+        public string Ship { get; set; }
+
+        //[ForeignKey(nameof(Origin))]
+        public virtual string OriginName { get; set; }
+        public string CrewName { get; set; }
+        public string CrewRole { get; set; }
+        public string CrewDescription { get; set; }
+        public string CrewMembers { get; set; }
+
+
+    }
+}
