adafa1a16fab651cfd2c9a2848e33840f48bcfbc
avery finchum <finchum.avery@gmail.com>
2022-04-12 16:32:43 -0400

    crew controller

---
+++ SP.WebApi/Controllers/CrewController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using SP.Models;
+using SP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SP.WebApi.Controllers
+{
+    [Authorize]
+    public class CrewController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            CrewService crewService = CreateCrewService();
+            var crews = crewService.GetCrews();
+            return Ok(crews);
+        }
+
+        public IHttpActionResult Post(CrewCreate crew)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreateCrewService();
+
+            if (!service.CreateCrew(crew))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+
+        private CrewService CreateCrewService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var crewService = new CrewService(userId);
+            return crewService;
+
+        }
+    }
+}
