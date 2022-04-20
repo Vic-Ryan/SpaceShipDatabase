f06a7ed7ed291151f164bf99e017aa633f70e9c8
Avery Finchum <finchum.avery@gmail.com>
2022-04-20 12:13:15 -0400

    joining crew ship model

--- SP.Models/JoiningCrewShip.cs
+++ SP.Models/JoiningCrewShip.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using SP.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SP.Models
 {
-    internal class JoiningCrewShip
+    public class JoiningCrewShip
     {
+        //Junction Table
+        [Key]
+        public int CrewShipId { get; set; }
+
+        [Required]
+        public string RoleOnShip { get; set; }
+
+        [Required] 
+        public string Description { get; set; }
+
+        [Required] 
+        [ForeignKey(nameof(Crew))]
+        public virtual Crew Ship { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Ship))]
+        public virtual string ShipName { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTimeOffset Created_At { get; set; }
+
+
+
     }
 }
